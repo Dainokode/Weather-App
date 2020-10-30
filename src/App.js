4e939e4f72e252b269252ebc9de1ac866f656aa1
Dainokode <antoniodainok@gmail.com>
2020-10-30 18:58:55 +0100

    app setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  fetchPosts();
+
   return (
     <div>
-      hi
+      app setup
     </div>
   )
 }
