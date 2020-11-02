9ad49d6762ef965e611649e90427c7d3a2f75dee
Dainokode <antoniodainok@gmail.com>
2020-11-02 15:57:37 +0100

    react countup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./styles/main.scss";
+import CountUp from 'react-countup';
 
 const App = () => {
 
   return (
     <div>
-      app setup
+      <h1><CountUp end={100} duration={4} /></h1>
     </div>
   )
 }
